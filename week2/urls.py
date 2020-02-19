bfc4046d7ca3bc8b0a645dcb4062d0b06d4aa8ef
Sharon Bryant <ssb7xx@virginia.edu>
2020-02-19 00:06:59 -0500

    added about page url and view

--- week2/urls.py
+++ week2/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('',include('week2_app.urls')),
     path('admin/', admin.site.urls),
+    path('about/', include('week2_app.urls')),
 ]
--- week2_app/views.py
+++ week2_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'week2_app/index.html')
+
+def about(request):
+    return render(request, 'week2_app/about.html')
