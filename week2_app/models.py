45beeff8bff5454f06454588f4e74795a3a047da
Sharon Bryant <ssb7xx@virginia.edu>
2020-02-19 03:06:25 -0500

    Added genre and movie models and made index html dynamically add database values for genre

--- week2_app/models.py
+++ week2_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=300)
+    genre_amount = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.genre_name
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=200)
+    movie_director = models.CharField(max_length=200)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie_title
--- week2_app/views.py
+++ week2_app/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import Genre, Movie
 # Create your views here.
 def index(request):
-    return render(request, 'week2_app/index.html')
+    genre_list = Genre.objects.all()
+    return render(request, 'week2_app/index.html', {'genres':genre_list})
 
 def about(request):
     return render(request, 'week2_app/about.html')
